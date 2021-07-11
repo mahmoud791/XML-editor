6ef4ba76d221732b229c5cbd6c00d875e9a83041
Mahmod 3ashry <64699750+mahmoud791@users.noreply.github.com>
2021-07-11 13:08:44 +0200

    Create GUI.py
    created the main window

---
+++ GUI.py
@@ -0,0 +1,38 @@
+from tkinter import*
+
+
+window = Tk()
+
+window.title("XML Editor")
+window.geometry('250x300')
+
+lbl = Label(window, text="Path: ")
+lbl.grid(column=0, row=0)
+
+pathentry = Entry(window,width=30)
+pathentry.grid(row=0,column=1)
+
+validate_button = Button(window, text='check consistensy',width=15)
+validate_button.grid(row=1,column=1)
+
+
+prettify_Button = Button(window, text='Prettify',width=15)
+prettify_Button.grid(row=2,column=1)
+
+
+Convert_to_JSON = Button(window, text='convert to JSON',width=15)
+Convert_to_JSON.grid(row=3,column=1)
+
+minify_Button = Button(window, text='Minify',width=15)
+minify_Button.grid(row=4,column=1)
+
+compress_Button = Button(window, text='Compress',width=15)
+compress_Button.grid(row=5,column=1)
+
+
+
+
+
+
+
+window.mainloop()
